e5f7a2bab7b14462abcfdf872518c9609c9d207e
Eduardo Ocaranza <edu_ocaranza@hotmail.com>
2023-10-09 15:17:55 -0300

    Error Handling

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -3,13 +3,17 @@ import { EError } from "../enums/EError.js";
 export const errorHandler = (error, req, res, next) =>{
     switch(error.code){
         case EError.AUTH_ERROR:
-            res.status(401).json({status: "error", error: error.cause});
+            res.status(401).send({status: "error", error: error.cause});
+            break;
         case EError.INVALID_JSON:
-            res.status(401).json({status: "error", error: error.cause});
+            res.status(401).send({status: "error", error: error.cause});
+            break;
         case EError.DATABASE_ERROR:
-            res.status(500).json({status: "error", error: error.message});
+            res.status(500).send({status: "error", error: error.message});
+            break;
         default:
-            res.status(500).json({status: "error", error: "error desconocido"});
+            res.status(500).send({status: "error", error: "error desconocido"});
+            break;
     }
     next(error);
 };
